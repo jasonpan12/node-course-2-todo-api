db561545380d7fd45e9394175afad2d684c0b447
Jason Pan <jpan@box.com>
2018-12-14 07:22:00 -0600

    fixed master branch

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,19 @@
 
 var env = process.env.NODE_ENV || 'development';
 
-if (env==='development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') { // test environment used in npm test
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') { // if env = development OR env = test
+  var config = require('./config.json');
+  var envConfig = config[env]; // bracket notation to access a property with var
+   Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key]; // for each key in the env, set process.env[key]
+    // this is how we get config variables as environment variables
+  }); // returns keys as an array
 }
+//
+// if (env==='development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') { // test environment used in npm test
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
